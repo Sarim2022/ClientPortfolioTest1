64d17ced5703928e736e25a889652227f60feaa5
Sarim <t-sarim.hasan@ocltp.com>
2026-03-25 23:03:54 +0530

    new work changes

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ const finalValue = "SHIP.";
 const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let iterations = 0;
 
-const interval = setInterval(() => {
-    targetElement.innerText = finalValue.split("")
-        .map((letter, index) => {
-            if(index < iterations) {
-                return finalValue[index];
-            }
-            return chars[Math.floor(Math.random() * 26)];
-        })
-        .join("");
-    
-    if(iterations >= finalValue.length) {
-        clearInterval(interval);
-    }
-    
-    iterations += 1 / 3; // Adjust speed here
-}, 40);
+const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+
+if (targetElement && !prefersReducedMotion) {
+    const interval = setInterval(() => {
+        targetElement.innerText = finalValue
+            .split("")
+            .map((letter, index) => {
+                if (index < iterations) {
+                    return finalValue[index];
+                }
+                return chars[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+
+        if (iterations >= finalValue.length) {
+            clearInterval(interval);
+        }
+
+        iterations += 1 / 3;
+    }, 40);
+} else if (targetElement) {
+    targetElement.innerText = finalValue;
+}
 const menuToggle = document.getElementById('menuToggle');
 const mobileOverlay = document.getElementById('mobileOverlay');
 const closeMenu = document.getElementById('closeMenu');
